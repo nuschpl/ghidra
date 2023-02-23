nuschpl/ghidra

commit 04134e06c36c6265af9bd33535a828d811bc46db

OMF Format: Fix COMMENT_CLASS_LIBMOD handling

Ghidra/Features/Base/src/main/java/ghidra/app/util/bin/format/omf/OmfCommentRecord.java
@@ -36,12 +36,17 @@ public class OmfCommentRecord extends OmfRecord {
 		readRecordHeader(reader);
 		commentType = reader.readNextByte();
 		commentClass = reader.readNextByte();
-		byte[] bytes = reader.readNextByteArray(
-			getRecordLength() - 3 /* 3 = sizeof(commentType+commentClass+trailing_crcbyte*/);
 
-		if (commentClass == COMMENT_CLASS_TRANSLATOR || commentClass == COMMENT_CLASS_LIBMOD ||
-			commentClass == COMMENT_CLASS_DEFAULT_LIBRARY) {
+		switch(commentClass) {
+		case COMMENT_CLASS_TRANSLATOR:
+		case COMMENT_CLASS_DEFAULT_LIBRARY:
+			byte[] bytes = reader.readNextByteArray(
+				getRecordLength() - 3 /* 3 = sizeof(commentType+commentClass+trailing_crcbyte*/);
 			value = new String(bytes, StandardCharsets.US_ASCII); // assuming ASCII
+			break;
+		case COMMENT_CLASS_LIBMOD:
+			value = readString(reader);
+			break;
 		}
 		readCheckSumByte(reader);
 	}

commit 00fe79de9a721578cd34460cd2b1738e8d8bb219

Fix problem introduced by #5016

Ghidra/Features/Base/src/main/java/ghidra/app/util/bin/format/omf/OmfCommentRecord.java
@@ -47,6 +47,9 @@ public class OmfCommentRecord extends OmfRecord {
 			case COMMENT_CLASS_LIBMOD:
 				value = readString(reader);
 				break;
+			default:
+				reader.setPointerIndex(reader.getPointerIndex() + getRecordLength() - 3);
+				break;
 		}
 		readCheckSumByte(reader);
 	}